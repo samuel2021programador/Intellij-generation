9fb418199fd7732655386c249e048eb506288410
samuel2021programador <samuelsaturno835@gmail.com>
2022-12-22 18:48:12 -0300

    msg aqui

---
+++ src/POO/nome.java
@@ -0,0 +1,10 @@
+package POO;
+
+public class nome {
+    public static void main(String args[])
+    {
+
+        pessoa pessoa1 = new pessoa ("Deus","Jesus","Samuel");
+        System.out.println(pessoa1.getNomeCompleto());
+    }
+}
